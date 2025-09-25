ff1d7fcd67266eb713544406d671a330d01842b0
Leo Li <leo.wj.li@gmail.com>
2025-09-24 17:23:49 -0700

    fix deployment errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Transition from "@/components/transitions";
+import Transition from "@/components/Transitions";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 
 const geistSans = localFont({
